6cda7754bb94c57679559f107358911489c5591b
Janneman <Jan@kooperativ.net>
2019-10-17 14:06:03 +0200

    Added Login/Security

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 let express = require("express")
 let mongodb = require('mongodb')
-
+let sanitizeHTML = require('sanitize-html')
 let app = express()
 let db
 
@@ -14,6 +14,18 @@ mongodb.connect(connectString,{useNewUrlParser: true},function(err, client){
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
+function passwordProtected(req,res,next){
+  res.set('WWW-Authenticate','Basic realm="Jans not to do App"')
+  console.log(req.headers.authorization)
+  if(req.headers.authorization == "Basic bGVhcm46bGVhcm5KUw==") {
+    next()
+  } else{
+    res.status(401).send("Authentication required")
+  }
+}
+
+app.use(passwordProtected)
+
 app.get('/', function (req,res){
   db.collection('items').find().toArray(function(err, items){
     res.send(`<!DOCTYPE html>
@@ -21,7 +33,7 @@ app.get('/', function (req,res){
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Simple To-Do App</title>
+    <title>Jans not to do App</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
   </head>
   <body>
@@ -37,20 +49,13 @@ app.get('/', function (req,res){
         </form>
       </div>
 
-      <ul id="Item-list class="list-group pb-5">
-       ${items.map(function(item){
-         return `<li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
-           <span class="item-text">${item.text}</span>
-           <div>
-             <button data-id="${item._id}" class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
-             <button data-id="${item._id}" class="delete-me btn btn-danger btn-sm">Delete</button>
-           </div>
-         </li>`
-       }).join('')}
-
+      <ul id="item-list" class="list-group pb-5">
       </ul>
 
     </div>
+  <script>
+  let items = ${JSON.stringify(items)}
+  </script>
   <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
   <script src='/browser.js'></script>
   </body>
@@ -59,13 +64,15 @@ app.get('/', function (req,res){
 })
 
 app.post('/create-item', function(req,res){
-  db.collection('items').insertOne({text: req.body.item}, function(){
-    res.redirect('/')
+  let safeText= sanitizeHTML(req.body.text,{allowedTags: [], allowedAttributes: {}})
+  db.collection('items').insertOne({text: safeText}, function(err, info){
+    res.json(info.ops[0])
   })
 })
 
 app.post('/update-item', function(req, res){
-  db.collection('items').findOneAndUpdate({_id: new mongodb.ObjectId(req.body.id)}, {$set: {text: req.body.text}}, function() {
+  let safeText= sanitizeHTML(req.body.text,{allowedTags: [], allowedAttributes: {}})
+  db.collection('items').findOneAndUpdate({_id: new mongodb.ObjectId(req.body.id)}, {$set: {text: safeText}}, function() {
     res.send("Sucsess")
   })
 })
